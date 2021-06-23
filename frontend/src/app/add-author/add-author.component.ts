1c0d8a43e958e2f62d351304c04b07fc2d416917
anntresaroy <81476619+anntresaroy@users.noreply.github.com>
2021-06-23 09:34:16 +0530

    Add files via upload

---
+++ frontend/src/app/add-author/add-author.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DetailsService } from '../details.service';
+
+@Component({
+  selector: 'app-add-author',
+  templateUrl: './add-author.component.html',
+  styleUrls: ['./add-author.component.css']
+})
+export class AddAuthorComponent implements OnInit {
+
+
+
+  title: String = "Add Author";
+
+  newAuthor = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    image: new FormControl('')
+  });
+
+  constructor(private detailsService:DetailsService, public router:Router) { }
+  ngOnInit(): void {
+  }
+
+  addAuthor(){
+    console.log(this.newAuthor.value);
+    this.detailsService.addAuthor(this.newAuthor.value);
+    console.log("Added Successfully");
+    alert("Author has been successfully added to the Library");
+    this.router.navigate(['/authors']);
+  }
+}
+
+  // newBook = new BookModel('','','','','');
